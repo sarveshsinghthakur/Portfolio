f1a491b8e9055bee1c9d2196f6856b55eb4b57ae
LOOP  ∞ <175203647+sarveshsinghthakur@users.noreply.github.com>
2024-09-28 15:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var typed = new Typed('#web', {
-    strings: ['Web Developer', 'Web Designer', 'Software Developer', 'Student'],
-    typeSpeed: 80,
-    backSpeed: 50,
+    strings: ['Web Developer', 'Web Designer', 'Software Developer', 'Python Developer', 'Student'],
+    typeSpeed: 30,
+    backSpeed: 30,
     loop: true
   });
+  
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.childnav2');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('show');
+});
+
+
